7aadc611151dd994fbecc23bdc1a54b218dd3ed9
hircjusz <darek.uchman@gmail.com>
2015-03-25 19:58:09 +0100

    Zalozenie/inicjalizacja SymTab

---
+++ Intermediate/Symbols/SymTab.cs
@@ -0,0 +1,38 @@
+﻿namespace Intermediate.Symbols
+{
+
+    public class SymTab : ISymTab
+    {
+
+
+    }
+
+    public interface ISymTab
+    {
+
+    //     /**
+    // * Getter.
+    // * @return the scope nesting level of this entry.
+    // */
+    //public int getNestingLevel();
+ 
+    ///**
+    // * Create and enter a new entry into the symbol table.
+    // * @param name the name of the entry.
+    // * @return the new entry.
+    // */
+    //public SymTabEntry enter(String name);
+ 
+    ///**
+    // * Look up an existing symbol table entry.
+    // * @param name the name of the entry.
+    // * @return the entry, or null if it does not exist.
+    // */
+    //public SymTabEntry lookup(String name);
+ 
+    ///**
+    // * @return a list of symbol table entries sorted by name.
+    // */
+    //public ArrayList<SymTabEntry> sortedEntries()
+    }
+}
---
+++ Intermediate/Symbols/SymTabEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Symbols
+{
+    public class SymTabEntry : ISymTabEntry
+    {
+
+
+    }
+
+    public interface ISymTabEntry
+    {
+    //    /**
+    //* Getter.
+    //* @return the name of the entry.
+    //*/
+    //    public String getName();
+
+    //    /**
+    //     * Getter.
+    //     * @return the symbol table that contains this entry.
+    //     */
+    //    public SymTab getSymTab();
+
+    //    /**
+    //     * Append a source line number to the entry.
+    //     * @param lineNumber the line number to append.
+    //     */
+    //    public void appendLineNumber(int lineNumber);
+
+    //    /**
+    //     * Getter.
+    //     * @return the list of source line numbers.
+    //     */
+    //    public ArrayList<Integer> getLineNumbers();
+
+    //    /**
+    //     * Set an attribute of the entry.
+    //     * @param key the attribute key.
+    //     * @param value the attribute value.
+    //     */
+    //    public void setAttribute(SymTabKey key, Object value);
+
+    //    public Object getAttribute(SymTabKey key)
+
+    }
+}
